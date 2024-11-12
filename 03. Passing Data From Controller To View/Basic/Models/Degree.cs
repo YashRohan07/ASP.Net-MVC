58d49063ba260c94dec0e5615f9c6f64cfed9807
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2024-11-13 02:44:38 +0600

    .

---
+++ 03. Passing Data From Controller To View/Basic/Models/Degree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Basic.Models
+{
+    public class Degree
+    {
+        public string Title { get; set; }
+        public string Institute { get; set; }
+        public string Year { get; set; }
+        public string Result { get; set; }
+    }
+}
