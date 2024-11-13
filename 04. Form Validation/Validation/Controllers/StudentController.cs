2c8f0e29b07509a8fce6b4460fd333b42aa7c2c8
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2024-11-13 10:00:16 +0600

    .

---
+++ 04. Form Validation/Validation/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            // Pass the success message from TempData to the View
+            ViewBag.Message = TempData["Message"];
+            return View(new Student());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                // After processing the create form, set a success message in TempData
+                TempData["Message"] = "Your data has been successfully sent!";
+                return RedirectToAction("Create"); // Redirect to show the success message on the Create page
+
+                //return RedirectToAction("Index", "Home"); // Redirect to the "Index" action of the "Home" controller after successfull
+            }
+
+            return View(s); // If the model is invalid, return the same view with validation errors
+        }
+    }
+
+}
---
+++ 04. Form Validation/Validation/Models/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Validation.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Please provide a name")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Name can only contain alphabetic characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please provide an Id")]
+        [RegularExpression(@"^\d{2}-\d{5}-\d{1,3}$", ErrorMessage = "Id format must be xx-xxxxx-x")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Please provide a date of birth")]
+        [CustomValidation(typeof(StudentValidation), "ValidateDob")]
+        public DateTime Dob { get; set; }
+
+        [Required(ErrorMessage = "Please provide an email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [CustomValidation(typeof(StudentValidation), "ValidateEmail")]
+        public string Email { get; set; }
+    }
+
+    public static class StudentValidation
+    {
+        public static ValidationResult ValidateDob(DateTime dob, ValidationContext context)
+        {
+            var age = DateTime.Now.Year - dob.Year;
+
+            if (age <= 20)
+                return new ValidationResult("Age must be greater than 20.");
+            return ValidationResult.Success;
+        }
+
+        public static ValidationResult ValidateEmail(string email, ValidationContext context)
+        {
+            var student = (Student)context.ObjectInstance; // Get the current student instance
+            var expectedEmail = $"{student.Id}@student.aiub.edu"; // Construct the expected email
+
+            // Check if the email matches the expected format
+            if (email != expectedEmail)
+                return new ValidationResult($"Email format must be {expectedEmail}.");
+
+            return ValidationResult.Success;
+        }
+    }
+}
